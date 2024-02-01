5707b433baaa2e7c0e2c98129116db2e1a9b94de
PedroPaulo <pppedro95@gmail.com>
2024-01-31 21:11:43 -0300

    Primeiro commit no branch main

---
+++ src/app/view/jogadores/cadastrar/cadastrar.page.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
+import { Posicao } from 'src/app/model/entities/Enum';
+import Jogador from 'src/app/model/entities/Jogador';
+import { AuthService } from 'src/app/model/services/auth.service';
+import { FirebaseService } from 'src/app/model/services/firestore.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.page.html',
+  styleUrls: ['./cadastrar.page.scss'],
+})
+export class CadastrarPage  implements OnInit {
+  public formCadastrarJ: FormGroup;
+
+  public nome! :string;
+  public idade! : number;
+  public altura! : number;
+  public peso! : number;
+  public universidade! : string;
+  public posicao! : Posicao;
+  public imagem: any;
+  public user: any;
+
+  constructor(private alert: AlertService,
+    private router : Router,
+    private firebase: FirebaseService, private auth: AuthService, private formBuilder: FormBuilder,
+    )  {
+      this.user = this.auth.getUserLogged();
+      this.formCadastrarJ = new FormGroup({
+      nome: new FormControl(''),
+      idade: new FormControl(''),
+      altura: new FormControl(''),
+      peso: new FormControl(''),
+      universidade: new FormControl(''),
+      posicao: new FormControl('')
+      })
+     }
+
+  ngOnInit() {
+    this.formCadastrarJ = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z\s-]+$/)]],
+      idade: ['', [Validators.required, Validators.min(16), Validators.max(42), Validators.pattern(/^[0-9]*$/)]],
+      altura: ['', [Validators.required, Validators.min(170), Validators.max(240), Validators.pattern(/^[0-9]*$/)]],
+      peso: ['', [Validators.required, Validators.min(70), Validators.max(200), Validators.pattern(/^[0-9]*$/)]],
+      universidade: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z\s-]+$/)]],
+      posicao: ['', Validators.required]
+    });
+  }
+
+  public uploadFile(imagem: any){
+    this.imagem = imagem.files;
+  }
+
+  get errorControl() {
+    return this.formCadastrarJ.controls;
+  }
+
+  submitForm(): boolean {
+    if (!this.formCadastrarJ.valid) {
+      this.alert.presentAlert('Erro', 'Erro ao preencher o formulário');
+      return false;
+    } else {
+      this.cadastrar();
+      return true;
+    }
+  }
+
+
+  cadastrar(){
+    if(this.formCadastrarJ.valid){
+       let novo : Jogador = new Jogador(this.formCadastrarJ.value['nome'], this.formCadastrarJ.value['idade'], this.formCadastrarJ.value['altura'], this.formCadastrarJ.value['peso'], this.formCadastrarJ.value['universidade'], this.formCadastrarJ.value['posicao']);
+       novo.uid = this.user.uid;
+       if (this.imagem) {
+        this.firebase.uploadImage(this.imagem, novo);
+        this.alert.presentAlert('Sucesso', 'Jogador salvo!');
+        this.router.navigate(['/home']);
+      } else {
+        this.firebase
+          .create(novo)
+          .then(() => {
+            this.alert.presentAlert('Sucesso', 'Jogador salvo!');
+            this.router.navigate(['/home']);
+          })
+          .catch((error) => {
+            console.error('Erro ao salvar o jogador', error);
+          });
+      }
+    } else {
+      this.alert.presentAlert(
+        'Erro',
+        'Preencha todos os campos obrigatórios corretamente!'
+      );
+    }
+   }
+   
+
+}
---
+++ src/app/view/jogadores/detalhar/detalhar.page.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
+import { Posicao } from 'src/app/model/entities/Enum';
+import Jogador from 'src/app/model/entities/Jogador';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/model/services/auth.service';
+import { FirebaseService } from 'src/app/model/services/firestore.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-detalhar',
+  templateUrl: './detalhar.page.html',
+  styleUrls: ['./detalhar.page.scss'],
+})
+export class DetalharPage implements OnInit {
+  public formEditar: FormGroup;
+
+
+  nome!: string;
+  idade!: number;
+  altura!: number;
+  peso!: number;
+  universidade!: string;
+  posicao: string;
+  jogador: Jogador;
+  indice: number;
+  edicao: boolean = true;
+  imagem!: any;  
+  user: any;
+
+  constructor(
+    private alert: AlertService,
+    private router : Router,
+    private firebase: FirebaseService, private alertController: AlertController, private auth: AuthService, private formBuilder: FormBuilder
+  ) {
+    this.user = this.auth.getUserLogged();
+  }
+
+  ngOnInit() {
+    this.jogador = history.state.jogador;
+    this.formEditar = this.formBuilder.group({
+      nome: [this.jogador.nome, [Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z\s-]+$/)]],
+      idade: [this.jogador.idade, [Validators.required, Validators.min(16), Validators.max(42), Validators.pattern(/^[0-9]*$/)]],
+      altura: [this.jogador.altura, [Validators.required, Validators.min(170), Validators.max(240), Validators.pattern(/^[0-9]*$/)]],
+      peso: [this.jogador.peso, [Validators.required, Validators.min(70), Validators.max(200), Validators.pattern(/^[0-9]*$/)]],
+      universidade: [this.jogador.universidade, [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z\s-]+$/)]],
+      posicao: [this.jogador.posicao, Validators.required]
+    });
+  }
+  
+  uploadFile(imagem: any){
+    this.imagem = imagem.files;
+  }
+
+
+  habilitar() {
+    if (this.edicao) {
+      this.edicao = false;
+    } else {
+      this.edicao = true;
+    }
+  }
+
+  editar() {
+    if (this.formEditar.valid) {
+      let novo : Jogador = new Jogador(this.formEditar.value.nome, this.formEditar.value.idade, this.formEditar.value.altura, this.formEditar.value.peso, this.formEditar.value.universidade, this.formEditar.value.posicao);
+      novo.id = this.jogador.id;
+      novo.uid = this.user.uid;
+      if (this.imagem) {
+        this.firebase.uploadImage(this.imagem, novo);
+        this.alert.presentAlert('Sucesso', 'Jogador alterado com sucesso!');
+        this.router.navigate(['/home']);
+      } else {
+        this.firebase
+          .update(novo, this.jogador.id)
+          .then(() => {
+            this.alert.presentAlert(
+              'Sucesso',
+              'Jogador alterado com sucesso!'
+            );
+            this.router.navigate(['/home']);
+          })
+          .catch((error) => {
+            console.error('Erro ao atualizar o jogador', error);
+          });
+      }
+    } else {
+      this.alert.presentAlert(
+        'Erro',
+        'Preencha todos os campos obrigatórios corretamente!'
+      );
+    }
+  }
+  
+
+  async confirmarEdicao() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Você tem certeza que deseja editar este jogador?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.editar();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async confirmarExclusao() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Você tem certeza que deseja apagar este jogador?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.excluir();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+
+  excluir() {
+    this.firebase.delete(this.jogador);
+    this.router.navigate(['/home']);
+  }
+}
